d5811181c5c957835b21f52b4dfaf02995de817b
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 15:21:07 +0900

    define Boards#params_board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,8 +3,14 @@ class BoardsController < ApplicationController
   end
 
   def create
-    @board = Board.new(params)
+    @board = Board.new(params_board)
 
     raise Exception, @board.inspect
+  end
+
+  private
+
+  def params_board
+    params.permit(:name, :from, :email, :content)
   end
 end
