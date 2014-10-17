a9bb6998b496c2695e54bec11e0e376033ffb172
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 20:48:39 +0900

    define comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @board = Board.find(params[:board_id])
+    @comment = Comment.new(params_comment)
+    @comment.board = @board
+
+    if @comment.save
+      redirect_to board_url(@board)
+    else
+      render "boards/show"
+    end
+  end
+
+  private
+
+  def params_comment
+    params.require(:comment).permit(:from, :email, :content)
+  end
 end
