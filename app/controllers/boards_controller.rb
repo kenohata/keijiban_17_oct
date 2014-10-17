c28190ba204bfbcf54048b3adc72e39f3590b617
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 20:51:48 +0900

    update boards#show

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -16,7 +16,7 @@ class BoardsController < ApplicationController
   end
 
   def show
-    @board = Board.find(params[:id])
+    @board = Board.includes(:comments).find(params[:id])
     @comment = Comment.new
   end
 
