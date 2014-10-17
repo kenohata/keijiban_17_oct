5bc467ff5d3a2443c5c358262bd48b2a9c1c35c7
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 20:38:11 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017061139) do
+ActiveRecord::Schema.define(version: 20141017113735) do
 
   create_table "boards", force: true do |t|
     t.string   "name"
@@ -21,5 +21,16 @@ ActiveRecord::Schema.define(version: 20141017061139) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.integer  "board_id"
+    t.string   "from"
+    t.string   "email"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["board_id"], name: "index_comments_on_board_id"
 
 end
