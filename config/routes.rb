111977c2b3c4abe34bb9f4e1fa9c9d5c29e3ed9d
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 15:28:42 +0900

    get 'boards/:id' => 'boards#show'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'boards' => 'boards#index'
   post 'boards' => 'boards#create'
+  get 'boards/:id' => 'boards#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
