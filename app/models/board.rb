1a96726ba61603abc95b6a7d2f38550f9c246005
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 15:21:39 +0900

    add validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ActiveRecord::Base
+  validates :name, presence: true, length: { in: 4..127 }
+  validates :from, presence: true
+  validates :email, presence: true
+  validates :content, presence: true, length: { in: 1..1000 }
 end
