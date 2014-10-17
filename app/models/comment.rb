0cc7275cba063f3ead021072f934e9cfe30753d2
Ken Ohata <ohata@daraf.co.jp>
2014-10-17 20:47:05 +0900

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :board
+
+  validates :board, presence: true
+  validates :from, presence: true
+  validates :email, presence: true
+  validates :content, presence: true, length: { in: 1..1000 }
 end
